04791efcb45e8e1b18aaac878682a2509c9c7fe3
Matthew Treadwell <Treadwell@inventati.org>
2020-12-16 00:06:04 -0800

    MacOS Dark Mode theme

--- img/logo.sh
+++ img/logo.sh
@@ -3,6 +3,6 @@
 IMG=$(basename $1 .png)
 
 echo "Resizing image to 200x100"
-convert $IMG.png -resize 200x100 $IMG.png
+convert $1 -resize 200x100 $IMG.png
 echo "Converting image to webp"
 cwebp $IMG.png -o $IMG.webp
